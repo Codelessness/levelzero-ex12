b5a53566ae38dbc13bd72af1f85c2f0a84f52d1e
Ashutosh Aman <ashutoshsingh161216@gmail.com>
2021-12-12 18:43:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// ex12: club info about a superhero together
+
+var naagraz = {
+ name : "naagraaz",
+ power : "snake",
+ costumeColor : "green",
+ rank : "1",
+ 
+
+}
+
+var dhruv ={
+  name: "dhruv",
+  power: "racer",
+  costumeColor : "yellow",
+  rank: "5,"
+}
+
+
+console.log(naagraz.rank < dhruv.rank);
+console.log(naagraz.costumeColor);
+
+
+// its all about creating object and accesing them 
